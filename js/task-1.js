4537e377a6e9e6e4ac06fd539f6f52c8c70d54aa
Serhii Taran <serg.taran1970@gmail.com>
2024-09-09 19:49:34 +0300

    js-mod3

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+'use strickt';
+
+function slugify(title) {
+  title = title.toLowerCase();
+
+  title = title.replace(/\s+/g, '-');
+
+  return title;
+}
+
+console.log(slugify('Arrays for beginners'));
+console.log(slugify('English for developer'));
+console.log(slugify('Ten secrets of JavaScript'));
+console.log(slugify('How to become a JUNIOR developer in TWO WEEKS'));
